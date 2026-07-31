f17f99483c3e88fc9d4c844eafeec61898359813
Naman Singh <naman.2002.as@gmail.com>
2026-07-31 23:53:03 +0530

    Time: 1 ms (99.95%), Space: 76.7 MB (97.78%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int bestEnding = nums[0];;
+        int ans = nums[0];
+
+        for(int i=1;i<n;i++){
+            int v1 = bestEnding + nums[i];
+            int v2 = nums[i];
+
+            bestEnding = Math.max(v1,v2);
+            ans = Math.max(ans,bestEnding);
+        }
+
+        return ans;
+    }
+}
