0692d60571a0e098ae5864b9ff24beeace039c05
Naman Singh <naman.2002.as@gmail.com>
2026-08-04 21:02:19 +0530

    Time: 1 ms (1.16%), Space: 42.3 MB (17.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    HashMap<Integer,Integer> dp = new HashMap<>();
+    public int climbStairs(int n) {
+        int i=0;
+        return fun(i,n);
+    }
+
+    int fun(int i,int n){
+        if(i==n){
+            return 1;
+        }
+        if(i>n){
+            return 0;
+        }
+
+        if(dp.containsKey(i)){
+            return dp.get(i);
+        }
+
+        int a1 = fun(i+1,n);
+        int a2 = fun(i+2,n);
+        int ans = a1+a2;
+
+        dp.put(i,ans);
+        return ans;
+    }
+}
