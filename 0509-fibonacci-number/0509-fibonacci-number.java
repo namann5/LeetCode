14989d168999fcf658816abca4b8f760bad5534b
Naman Singh <naman.2002.as@gmail.com>
2026-08-06 22:06:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (53.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -4,13 +4,14 @@ class Solution {
             return n;
         }
 
-        int [] dp = new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
+       int prev2 = 0;
+       int prev1=1;
 
-        for(int i=2;i<=n;i++){
-            dp[i]= dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+       for(int i=2;i<=n;i++){
+        int cur = prev1 + prev2;
+        prev2= prev1;
+        prev1 = cur;
+       }
+       return prev1;
     }
 }
