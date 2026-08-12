411bd4570b9da61ed670bbe5e355dbbdb9c7ebae
Naman Singh <naman.2002.as@gmail.com>
2026-08-12 22:45:27 +0530

    Time: 90 ms (25.3%), Space: 108.7 MB (40.26%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int n = prices.length;
+        int k = 4;
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return fun(prices, n, 0, k, dp);
+    }
+
+    int fun(int[] a, int n, int i, int k, int[][] dp) {
+
+        if (i == n || k == 0) {
+            return 0;
+        }
+
+        if (dp[i][k] != -1) {
+            return dp[i][k];
+        }
+
+        if (k % 2 == 0) {
+
+            // Buy
+            int c1 = fun(a, n, i + 1, k - 1, dp) - a[i];
+
+            // Skip
+            int c2 = fun(a, n, i + 1, k, dp);
+
+            return dp[i][k] = Math.max(c1, c2);
+
+        } else {
+
+            // Sell
+            int c1 = fun(a, n, i + 1, k - 1, dp) + a[i];
+
+            // Skip
+            int c2 = fun(a, n, i + 1, k, dp);
+
+            return dp[i][k] = Math.max(c1, c2);
+        }
+    }
+}
