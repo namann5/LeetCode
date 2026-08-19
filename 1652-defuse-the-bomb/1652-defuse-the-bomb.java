5ddaeda33e574e63ddc281411170394e5e02c495
Naman Singh <naman.2002.as@gmail.com>
2026-08-19 23:45:50 +0530

    Time: 0 ms (100%), Space: 43.5 MB (92.7%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -4,47 +4,33 @@ class Solution {
         int n = code.length;
         int[] ans = new int[n];
 
-        if (k == 0) {
-            return ans;
-        }
+        if (k == 0) return ans;
 
         int sum = 0;
 
         if (k > 0) {
-
-            // Initial window: next k elements
             for (int i = 1; i <= k; i++) {
                 sum += code[i % n];
             }
 
             for (int i = 0; i < n; i++) {
-
                 ans[i] = sum;
 
-                // Remove old element
                 sum -= code[(i + 1) % n];
-
-                // Add next element
                 sum += code[(i + k + 1) % n];
             }
 
         } else {
-
             k = -k;
 
-            // Initial window: previous k elements
             for (int i = 1; i <= k; i++) {
                 sum += code[(n - i) % n];
             }
 
             for (int i = 0; i < n; i++) {
-
                 ans[i] = sum;
 
-                // Remove purana element
                 sum -= code[(i - k + n) % n];
-
-                // Add next previous element
                 sum += code[i];
             }
         }
