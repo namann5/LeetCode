c1b2bdeb74e475f9377f8507206b1839667a7be0
Naman Singh <naman.2002.as@gmail.com>
2026-08-24 22:09:31 +0530

    Time: 0 ms (100%), Space: 43.6 MB (89.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,14 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        return fun(l,h);
-    }
+        int i= m-1;
+        int j= n-1;
+        int k= m+n-1;
 
-    fun(int l,int h){
-        if(l<h){
-            mid = (l+h)/2;
+        while(i>= 0 && j >=0){
+            if(nums1[i] < nums2[j]){
+                nums1[k] = nums2[j];
+                j--;
+            }else{
+                nums1[k] = nums1[i];
+                i--;
+            } 
+            k--;
         }
-        fun(l,mid);
-        fun(mid+1,h);
-        fun(l,mid,h);
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
 }
