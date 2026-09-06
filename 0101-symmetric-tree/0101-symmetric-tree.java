2466d48b645cec3fb0857ec69cf282f946496c1e
Naman Singh <naman.2002.as@gmail.com>
2026-09-06 21:49:09 +0530

    Time: 0 ms (100%), Space: 43.4 MB (85.5%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -17,23 +17,14 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
 
-        return isSame(root.left ,root.right);
+        return mirror(root.left,root.right);
     }
+    private boolean mirror(TreeNode a, TreeNode b){
+        if(a == null && b == null) return true;
+        if(a == null || b == null) return false;
 
-    private boolean isSame(TreeNode r1, TreeNode r2){
-        if(r1== null && r2 == null) return true;
-
-        if(r1==null || r2==null){
-            return false;
-        }
-
-        if(r1.val != r2.val) return false;
-
-        boolean t1 = isSame(r1.left, r2.right);
-        boolean t2 =isSame(r1.right, r2.left);
-
-        return t1 && t2;
+        return a.val == b.val
+         && mirror(a.left,b.right) 
+         && mirror(a.right,b.left);
     }
-
-    
 }
