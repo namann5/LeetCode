940fa292a1da92f9fd8ae733b15cac374541475d
Naman Singh <naman.2002.as@gmail.com>
2026-09-14 00:21:28 +0530

    Time: 0 ms (100%), Space: 43.7 MB (27.15%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,35 +1,35 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] oG) {
-        if(oG[0][0] == 1) return 0;
-        oG[0][0] = 1;
+    public int uniquePathsWithObstacles(int[][] og) {
+        if(og[0][0] == 1) return 0;
+        og[0][0]=1;
 
-        int m = oG.length;
-        int n = oG[0].length;
+        int m= og.length;
+        int n= og[0].length;
 
         for(int i=1;i<m;i++){
-            if(oG[i][0]==1){
-                oG[i][0] = 0;
+            if(og[i][0]==1){
+                og[i][0]=0;
             }else {
-                oG[i][0]= oG[i-1][0];
+                og[i][0]= og[i-1][0];
             }
         }
         for(int j=1;j<n;j++){
-            if(oG[0][j]==1){
-                oG[0][j] =0;
+            if(og[0][j]==1){
+                og[0][j]=0;
             }else {
-                oG[0][j]= oG[0][j-1];
+                og[0][j]= og[0][j-1];
             }
         }
 
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(oG[i][j]==1){
-                    oG[i][j]= 0;
+                if(og[i][j]==1){
+                    og[i][j]=0;
                 }else {
-                    oG[i][j]= oG[i-1][j] + oG[i][j-1];
+                    og[i][j]= og[i-1][j]+ og[i][j-1];
                 }
             }
         }
-        return oG[m-1][n-1];
+        return og[m-1][n-1];
     }
 }
