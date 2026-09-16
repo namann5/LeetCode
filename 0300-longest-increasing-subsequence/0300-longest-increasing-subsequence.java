bfd63cf8afef999d8b1f11b5daf81e4ec0eccf3c
Naman Singh <naman.2002.as@gmail.com>
2026-09-16 10:47:40 +0530

    Time: 34 ms (75.98%), Space: 45.9 MB (83.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,17 +3,18 @@ class Solution {
         if(nums.length == 0) return 0;
         int n = nums.length;
         int[] dp = new int[n];
-        Arrays.fill(dp,1);
-        int max =1;
-
-         for(int i=1;i<n;i++){
+        dp[0]=1;
+        int ans =1;
+        for(int i=1;i<n;i++){
+            int max =0;
             for(int j=0;j<i;j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
+                if(nums[i]>nums[j]){
+                    max = Math.max(max,dp[j]);
                 }
             }
-            max = Math.max(dp[i], max);
-         }
-         return max;
+            dp[i]= max+1;
+            ans = Math.max(ans, dp[i]);
+        }
+        return ans;
     }
 }
